b49162048115d4853c302b8bb33c510582138648
JuniorDuarte93 <jr.duartee.93@gmail.com>
2018-03-24 20:35:37 -0300

    teste

---
+++ src/buscaia/No.java
@@ -0,0 +1,99 @@
+
+package buscaia;
+
+public class No {
+
+    int valor;
+    No noEsq, noDir;
+    
+    public static No raiz;
+
+    public No(int valor) {
+        this.valor = valor;
+    }
+    
+    public static void inserir(int valor) {
+        inserir(raiz, valor);
+    }
+    
+   
+
+    public static void inserir(No node, int valor) {
+        if (node == null) {
+            System.out.println("Raiz " + valor);
+            raiz = new No(valor);
+        } else {
+            if (valor < node.valor) {
+                if (node.noEsq != null) {
+                    inserir(node.noEsq, valor);
+                } else {
+                    System.out.println("Inserindo " + valor + " a esquerda de " + node.valor);
+                    node.noEsq = new No(valor);
+                }
+
+            } else {
+                if (node.noDir != null) {
+                    inserir(node.noDir, valor);
+                } else {
+                    System.out.println("Inserindo " + valor + " a direita de " + node.valor);
+                    node.noDir = new No(valor);
+                }
+            }
+        }
+        
+       
+    }
+    
+    public static void ordem(No node) {
+        if (node != null) {
+            ordem(node.noEsq);
+            System.out.print(node.valor + ", ");
+            ordem(node.noDir);
+
+        }
+    }
+    
+    
+      public static void buscaEmProfundidade(No node, int valorProcurado){
+          
+          
+        if (node == null) {
+            System.out.print("Arvore Vazia \n \n" );
+            
+        }else if(node.valor == valorProcurado) {
+           ordem(node.noEsq);
+           System.out.print("Achou" );
+           ordem(node.noDir);
+        }
+          
+          
+          
+        
+      
+    }
+    
+
+    
+   
+   
+    public static void main(String[] args) {
+        
+        buscaEmProfundidade(raiz, 50);
+        
+        inserir(4);
+        inserir(15);
+        inserir(5);
+        inserir(2);
+        inserir(50);
+        
+        ordem(raiz);
+        
+        
+         buscaEmProfundidade(raiz, 5);
+        
+        
+        
+    
+    }
+    
+}
